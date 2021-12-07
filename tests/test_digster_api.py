4e40bded6543fcd1a9d58f4566b7d60a6323a1fb
maxenceroux <maxence.roux@ornikar.com>
2021-12-07 18:45:45 +0100

    Add Flake8 lint - pytest tests -black formatting

--- tests/test_digster_api.py
+++ tests/test_digster_api.py
@@ -1,5 +1,11 @@
 from digster_api import __version__
+from digster_api.math import multiply_two_numbers
+
+
+def test_multiply_two_numbers():
+    result = multiply_two_numbers(2, 3)
+    assert result == 6
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
