7977be5883dce4cb1c1ebce9251794318d14e452
maxenceroux <maxence.roux@ornikar.com>
2021-12-11 12:49:35 +0100

    Add makefile

--- digster_api/models.py
+++ digster_api/models.py
@@ -12,3 +12,13 @@ class User(Base):
     )
     last_name = Column(String)
     age = Column(Integer)
+
+
+class User2(Base):
+    __tablename__ = "users_2"
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(
+        String,
+    )
+    last_name = Column(String)
+    age = Column(Integer)
